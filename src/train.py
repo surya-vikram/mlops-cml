83b86555d1f29e2cbb34fae2e9455c9862c4f5f0
Surya Vikram <suryavikramrajput@gmail.com>
2025-06-29 11:57:45 +0000

    update iter

--- src/train.py
+++ src/train.py
@@ -12,7 +12,7 @@ X = df.drop('species', axis=1)
 y = df['species']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# 3. Train for 200 iterations
+# 3. Train for 200 iterations and merge
 model = LogisticRegression(max_iter=200)
 model.fit(X_train, y_train)
 
