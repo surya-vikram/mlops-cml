7690d1424949e3882338947ea9acc60fd6895450
Surya Vikram <suryavikramrajput@gmail.com>
2025-06-29 11:27:57 +0000

    fix iter param

--- src/train.py
+++ src/train.py
@@ -13,7 +13,7 @@ y = df['species']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # 3. Train
-model = LogisticRegression(1)
+model = LogisticRegression(max_iter=1)
 model.fit(X_train, y_train)
 
 # 4. Save
